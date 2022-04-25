981c6d9f66b949c270729b3d9d331d69157b1845
Douglas <douglas-cprdev@hotmail.com>
2022-04-25 19:26:51 -0300

    (tests) add tests to add charity use case

--- api/src/app/__tests__/services/add-charity.spec.ts
+++ api/src/app/__tests__/services/add-charity.spec.ts
@@ -1,7 +1,7 @@
 import { AddCharity } from '@/domain/usecases'
 import { CharityRepositorySpy } from '@/app/__tests__/mocks'
 import { AddCharityService } from '@/app/services'
-import { validCharityData } from '@/domain/__tests__/entities/mocks'
+import { invalidCharityData, validCharityData } from '@/domain/__tests__/entities/mocks'
 import { right } from '@/shared'
 import { Charity } from '@/domain/entities'
 
@@ -23,8 +23,36 @@ describe('AddCharityService', () => {
   it('should return new charity on success', async () => {
     const { sut } = makeSut()
     const charityData = validCharityData()
+
     const response = await sut.add(charityData)
     
     expect(response.value).toBeInstanceOf(Charity)
+  })
+
+  it('should not return new charity with invalid params', async () => {
+    const { sut } = makeSut()
+    const charityData = invalidCharityData()
+
+    const response = await sut.add(charityData)
+
+    expect(response.value).not.toBeInstanceOf(Charity)
+  })
+
+  it('should call CharityRepositorySpy with correct params', async () => {
+    const { sut, charityRepositorySpy } = makeSut()
+    const charityData = validCharityData()
+
+    await sut.add(charityData)
+
+    expect(charityRepositorySpy.addCharityCalledWith).toEqual(charityData)
+  })
+
+  it('should throw if CharityRepositorySpy throws', async () => {
+    const { sut } = makeSut()
+    const charityData = invalidCharityData()
+
+    const response = await sut.add(charityData)
+
+    expect(response.isLeft()).toBe(true)
   })
 })
